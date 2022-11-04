01b2e11f54aeff73d864d90d6383040f6513335d
Therakar <matteo.pau200@gmail.com>
2022-11-04 14:24:05 +0100

    inserisce un ciclo while che, sfruttando la funzione getRndInteger per la generazione di numeri casuali, crea 5 numeri in un range da 1 a 100 senza ripetizioni

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 "use strict"
+
+//ARRAY
+//array che conterrà i 5 numeri 
+const numberList = [];
+
+//UTILITY FUNCTIONS
+//funzione creazione numeri
+function getRndInteger(min, max){
+    return Math.floor(Math.random()* (max - min + 1) + min);
+}
+
+// MAIN
+// questa è una funzione che crea 5 numeri in un range da 1 a 100 senza ripetizioni
+while (numberList.length < 5){
+    const numberRandom = getRndInteger(1, 100);
+    if(numberList.includes(numberRandom) === false){
+        numberList.push(numberRandom);
+    }
+}
+console.log(numberList);//DEBUG
