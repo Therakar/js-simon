89f4aa38e6784e825807ee19fe15a8679ea1837c
Therakar <matteo.pau200@gmail.com>
2022-11-05 19:22:18 +0100

    inserice una time out function della durata di 30 secondi al termine della quale verrà applicato un display none all'h2 che contiene i numeri nell'html

--- js/main.js
+++ js/main.js
@@ -15,15 +15,24 @@ function getRndInteger(min, max){
 const casualNumber = document.getElementById(`casualNumbers`);//seleziono il div con classe numberList
 // questa è una funzione che crea 5 numeri in un range da 1 a 100 senza ripetizioni
 while (numberList.length < 5){
-    const numberRandom = getRndInteger(1, 100); //richiamo la funzione che genera i numeri casuali e li assegno a una costante numebrRandom
-    if(numberList.includes(numberRandom) === false){ //questo ciclo for controlla che i numeri non si ripetano 
-        
-        numberList.push(numberRandom);//pusha i numeri all'interno dell'array
+     //richiamo la funzione che genera i numeri casuali e li assegno a una costante numebrRandom
+    const numberRandom = getRndInteger(1, 100);
+
+    //questo ciclo for controlla che i numeri non si ripetano 
+    if(numberList.includes(numberRandom) === false){ 
+
+        //pusha i numeri all'interno dell'arra
+        numberList.push(numberRandom);
     }
     console.log
-    casualNumber.innerHTML = numberList; //stampa l'array che contiene i numeri casuali nella pagina   
+    //stampa l'array che contiene i numeri casuali nella pagina   
+    casualNumber.innerHTML = numberList; 
 }
-    
+ //countdown di 30 secondi al termine del quale i numeri spariscono
+setTimeout(function(){
+    casualNumber.style.display = 'none';
+}, 30 * 1000)
+
 
 
 
