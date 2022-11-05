156da6db5c4c5e268c405d7c1d1c7f25c5e8ac57
Therakar <matteo.pau200@gmail.com>
2022-11-05 19:02:52 +0100

    stampa i numeri nell'html

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 //ARRAY
 //array che conterrà i 5 numeri 
-const numberList = [];
+let numberList = [];
 
 //UTILITY FUNCTIONS
 //funzione creazione numeri
@@ -10,12 +10,22 @@ function getRndInteger(min, max){
     return Math.floor(Math.random()* (max - min + 1) + min);
 }
 
+
 // MAIN
+const casualNumber = document.getElementById(`casualNumbers`);//seleziono il div con classe numberList
 // questa è una funzione che crea 5 numeri in un range da 1 a 100 senza ripetizioni
 while (numberList.length < 5){
-    const numberRandom = getRndInteger(1, 100);
-    if(numberList.includes(numberRandom) === false){
-        numberList.push(numberRandom);
+    const numberRandom = getRndInteger(1, 100); //richiamo la funzione che genera i numeri casuali e li assegno a una costante numebrRandom
+    if(numberList.includes(numberRandom) === false){ //questo ciclo for controlla che i numeri non si ripetano 
+        
+        numberList.push(numberRandom);//pusha i numeri all'interno dell'array
     }
+    console.log
+    casualNumber.innerHTML = numberList; //stampa l'array che contiene i numeri casuali nella pagina   
 }
-console.log(numberList);//DEBUG
+    
+
+
+
+
+
